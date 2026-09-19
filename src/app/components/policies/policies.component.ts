175594d814a92b102bfdd55d913d35f1f8d76363
Hamza Foy <hamzaoflouisville@gmail.com>
2026-09-19 02:40:58 -0400

    terms & conditions converted to CMS-able page.

--- src/app/components/policies/policies.component.ts
+++ src/app/components/policies/policies.component.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { LayoutService } from '../../services/layout.service';
+import { HttpClient } from '@angular/common/http';
 
+interface TermsConditionsContent {
+  title: string;
+  terms: Terms[];
+}
+
+interface Terms {
+  title: string;
+  content: string[];
+}
+  
 @Component({
   selector: 'app-policies',
   templateUrl: './policies.component.html',
   styleUrl: './policies.component.scss'
 })
 export class PoliciesComponent {
+  termsConditions?: TermsConditionsContent;
+  termsConditionsLoading = true;
+  termsConditionsError = false;
 
-  constructor(private layoutService: LayoutService) {}
+  constructor(
+    private layoutService: LayoutService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.http.get<TermsConditionsContent>('/content/terms_conditions.json')
+      .subscribe({
+        next: (termsConditions) => {
+          this.termsConditions = termsConditions;
+          this.termsConditionsLoading = false;
+        },
+        error: (error) => {
+          console.error('Unable to load terms and conditions:', error);
+          this.termsConditionsLoading = false;
+          this.termsConditionsError = true;
+        }
+      });
+  }
 
   //Layout Service Methods
   get IsMobileViewport() {
