520b886aa2c0a9710740473e896e4739de186035
Hamza Foy <hamzaoflouisville@gmail.com>
2026-09-19 22:14:18 -0400

    add faq page into Decap CMS

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 import { LayoutService } from '../../services/layout.service';
+import { HttpClient } from '@angular/common/http';
+
+interface FaqContent {
+  title: string;
+  questions: QnA[];
+}
+
+interface QnA {
+  question: string;
+  answer: string;
+}
 
 @Component({
   selector: 'app-faq',
@@ -7,8 +18,29 @@ import { LayoutService } from '../../services/layout.service';
   styleUrl: './faq.component.scss'
 })
 export class FaqComponent {
+  frequentlyAskedQuestions?: FaqContent;
+  faqLoading: boolean = true;
+  faqError: boolean = false;
 
-  constructor(private layoutService: LayoutService) {}
+  constructor(
+    private layoutService: LayoutService, 
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.http.get<FaqContent>('/content/faq.json')
+      .subscribe({
+        next: (faqContent) => {
+          this.frequentlyAskedQuestions = faqContent;
+          this.faqLoading = false;
+        },
+        error: (error) => {
+          console.error('Unable to load FAQ content:', error);
+          this.faqLoading = false;
+          this.faqError = true;
+        }
+      });
+  }
 
   //Layout Service Methods
   get IsMobileViewport() {
